cada5dbb67b8aba49276a9675cbd347f4ff3f23a
Damanu <e.schwarzhans@gmx.at>
2015-08-02 21:45:10 +0200

    small changes

--- Test1.py
+++ Test1.py
@@ -7,9 +7,17 @@ import os
 import sys
 
 def main(argv):
-
+#---------variables----------
+	T,U=symbols('T U')
 #----------argparse-----------------
 	parser=ArgumentParser()
 	parser.add_argument("--Kin","-T",action="store",dest="T",help="input should be the kinetic energy of the sysem"0)
 	parser.add_argument("--Pot","-U",action="store",dest="U",help="input should be the potential energy of the sysem")
+	
+	T=options.T
+	print T
 
+
+
+if __name__=="__main__"	#start main program
+	main(sys.argv)
